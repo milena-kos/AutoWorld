a40d993eb9c98fd7be12eb0a707349b26d30a849
milena-kos <75163549+milena-kos@users.noreply.github.com>
2021-01-12 19:11:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from pynput.keyboard import Key, Controller as KeyboardController
 from time import sleep
 import simpleaudio as sa
+
 keyboard = KeyboardController()
+
 f = open('country.txt', 'r')
+
 sleep(3)
+
 filename = 'cooldown.wav'
 wave_obj = sa.WaveObject.from_wave_file(filename)
 play_obj = wave_obj.play()
+
 sleep(2.8)
+
 for line in f:
     keyboard.type(line)
+
 play_obj.wait_done()
